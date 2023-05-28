e718a6468b12d21205a130e02c905dc4cccd152c
Tiago Barroso <tmbb@campus.ul.pt>
2023-05-28 16:20:45 +0100

    prepared for publication in hex.pm

--- mix.exs
+++ mix.exs
@@ -4,11 +4,24 @@ defmodule CodeGen.MixProject do
   def project do
     [
       app: :code_gen,
-      version: "0.1.0",
+      version: "0.2.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      elixirc_paths: elixirc_paths(Mix.env())
+      elixirc_paths: elixirc_paths(Mix.env()),
+      description: "Flexible code generation for Elixir projects",
+      package: package()
+    ]
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "code_gen",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/tmbb/ex_code_gen"}
     ]
   end
 
@@ -27,7 +40,7 @@ defmodule CodeGen.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.29", only: :dev},
-      {:makeup_eex, "~> 0.1.0"}
+      {:makeup_eex, "~> 0.1.0", only: :dev}
     ]
   end
 end
